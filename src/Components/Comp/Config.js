e4dbba866409044d0fd7bbf1b56e39833f9ebdeb
Ganesh Singh Bisht <60257659+Ganesh-Best@users.noreply.github.com>
2024-10-05 23:18:03 +0530

    Update Config.js

--- src/Components/Comp/Config.js
+++ src/Components/Comp/Config.js
@@ -1,7 +1,7 @@
 const  BASE_URL = "https://backend-course-selling-website.onrender.com";
 let production = true ;
  
- if(!production)
+ if(production)
     BASE_URL = "http://147.79.71.181:9000";
 
 module.exports = {
